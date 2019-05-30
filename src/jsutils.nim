a814869e8927981e61c3dd7ef1bea6c0c79c42d3
Jairo <kidandcat@gmail.com>
2019-05-30 09:37:27 +0200

    remove untyped

--- src/jsutils.nim
+++ src/jsutils.nim
@@ -1,8 +1,4 @@
-# This is just an example to get you started. A typical library package
-# exports the main API in this file. Note that you cannot rename this file
-# but you can remove it if you wish.
 import jsffi, asyncjs
 
-proc log*(anything: untyped) {.importcpp:"console.log(#)".}
 proc fetchText*(url: cstring): Future[cstring] {.importcpp: "fetch(#).then(r => r.text())".}
 proc fetchJson*(url: cstring): Future[JsObject] {.importcpp: "fetch(#).then(r => r.json())".}
