82e40add7619b3dc5b0387cc4c2115f463b95705
Jairo <kidandcat@gmail.com>
2019-06-14 10:48:06 +0200

    fetch config

--- src/jsutils.nim
+++ src/jsutils.nim
@@ -1,4 +1,6 @@
-import jsffi, asyncjs
+import jsffi, asyncjs, json
 
 proc fetchText*(url: cstring): Future[cstring] {.importcpp: "fetch(#).then(r => r.text())".}
 proc fetchJson*(url: cstring): Future[JsObject] {.importcpp: "fetch(#).then(r => r.json())".}
+proc fetchText*(url: cstring, config: JsonNode): Future[cstring] {.importcpp: "fetch(#, #).then(r => r.text())".}
+proc fetchJson*(url: cstring, config: JsonNode): Future[JsObject] {.importcpp: "fetch(#, #).then(r => r.json())".}
