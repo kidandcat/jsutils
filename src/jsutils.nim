9b50d68cbc0e43375b7ca2ce14f8bf7c34b26e5b
Jairo <kidandcat@gmail.com>
2019-06-21 00:33:51 +0200

    Update jsutils.nim

--- src/jsutils.nim
+++ src/jsutils.nim
@@ -4,3 +4,4 @@ proc fetchText*(url: cstring): Future[cstring] {.importcpp: "fetch(#).then(r => 
 proc fetchJson*(url: cstring): Future[JsObject] {.importcpp: "fetch(#).then(r => r.json())".}
 proc fetchText*(url: cstring, config: JsonNode): Future[cstring] {.importcpp: "fetch(#, #).then(r => r.text())".}
 proc fetchJson*(url: cstring, config: JsonNode): Future[JsObject] {.importcpp: "fetch(#, #).then(r => r.json())".}
+proc stringify*(c: JsObject): cstring {.importcpp:"JSON.stringify(#)".}
