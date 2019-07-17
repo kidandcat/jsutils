246e56af80fc6157f0733ad2e4d104de3c3ee903
Jairo Caro-Accino Viciana <kidandcat@gmail.com>
2019-07-17 10:35:02 +0200

    two more procs

--- src/jsutils.nim
+++ src/jsutils.nim
@@ -1,7 +1,20 @@
-import jsffi, asyncjs, json
+import jsffi, asyncjs, json, dom
 
 proc fetchText*(url: cstring): Future[cstring] {.importcpp: "fetch(#).then(r => r.text())".}
 proc fetchJson*(url: cstring): Future[JsObject] {.importcpp: "fetch(#).then(r => r.json())".}
 proc fetchText*(url: cstring, config: JsonNode): Future[cstring] {.importcpp: "fetch(#, #).then(r => r.text())".}
 proc fetchJson*(url: cstring, config: JsonNode): Future[JsObject] {.importcpp: "fetch(#, #).then(r => r.json())".}
 proc stringify*(c: JsObject): cstring {.importcpp:"JSON.stringify(#)".}
+
+proc addFavicon(href: string, kind = "image/png") =
+    var css = document.createElement("link")
+    css.setAttribute("href", href)
+    css.setAttribute("rel", "shortcut icon")
+    css.setAttribute("type", kind)
+    document.head.appendChild(css)
+
+proc addStylesheet(href: string) =
+    var css = document.createElement("link")
+    css.setAttribute("href", href)
+    css.setAttribute("rel", "stylesheet")
+    document.head.appendChild(css)
